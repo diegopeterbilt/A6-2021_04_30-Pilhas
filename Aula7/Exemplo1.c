7d51b70d0de61de99a00139045d85e8aa8932e0b
Diego de Oliveira <diego.oliveira@gmail.com>
2021-04-13 22:00:15 -0300

    código corrigido

--- Aula7/Exemplo1.c
+++ Aula7/Exemplo1.c
@@ -4,9 +4,7 @@
 
 typedef struct ElementoPrimitivo
 {
-    char *nome_processo;
-    int prioridade;
-    int tempo_de_execucao;
+    char *dado;
     struct ElementoPrimitivo *seguinte;
        
 }Elemento;
@@ -28,13 +26,14 @@ int Push_Pilha(Pilha *pilha, char *texto)
     Elemento *novo_elemento;
     if((novo_elemento = (Elemento*) malloc(sizeof(Elemento))) == NULL)
         return -1;
-    if ((novo_elemento->dado = (char *)malloc(10 * sizeof(char))) == NULL)
+    if ((novo_elemento->dado = (char * )malloc(10 * sizeof(char))) == NULL)
         return -1;
+
     strcpy(novo_elemento->dado, texto);
     novo_elemento->seguinte = pilha->inicio;
     pilha->inicio = novo_elemento;
     pilha->tamanho++;
-
+    return 0;
 }
 
 int Pop_Pilha(Pilha *pilha)
@@ -44,7 +43,7 @@ int Pop_Pilha(Pilha *pilha)
         return -1;
     remove_elemento = pilha->inicio;
     pilha->inicio = pilha->inicio->seguinte;
-    free(remove_elemento->seguinte);
+    free(remove_elemento->dado);
     free(remove_elemento);
     pilha->tamanho--;
     return 0;
@@ -63,7 +62,6 @@ void List_Pilha(Pilha *pilha)
         atual = atual->seguinte;
     }
     printf("-------- FUNDO DA PILHA--------\n");
-
 }
 
 void Size_Pilha( Pilha *pilha)
@@ -75,19 +73,20 @@ int main()
 {
     Pilha *teste1;
     char *aux;
-    int i;
+    int i = 0;
 
     if((teste1 = (Pilha *) malloc(sizeof(Pilha))) == NULL)
     return -1;
     Init_Pilha(teste1);
-
+    for ( i = 0; i < 3; i++)
+    {    
         printf("Digite alguma coisa com ate 10 letras: ");
-        scanf("%s", aux);
-        Push_Pilha(teste1, aux);
+        scanf("%s", &aux);
+        Push_Pilha(teste1, &aux);
         printf("A pilha tem elementos: %i\n\n", teste1->tamanho);
         List_Pilha(teste1);
+    }
 
     Pop_Pilha(teste1);
-    printf("A pilha tem elementos: %i\n\n", teste1->tamanho);
     List_Pilha(teste1);
 }
